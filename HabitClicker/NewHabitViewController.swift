f840daf3c8b7281d0bc7b30612d66b8789c9eb11
hongsii <hongsii.dev@gmail.com>
2020-02-01 11:15:21 +0900

    Add cancel event in new habit view

---
+++ HabitClicker/NewHabitViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewHabitViewController.swift
+//  HabitClicker
+//
+//  Created by red.bean on 2020/02/01.
+//  Copyright © 2020 hongsii. All rights reserved.
+//
+
+import UIKit
+
+class NewHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
